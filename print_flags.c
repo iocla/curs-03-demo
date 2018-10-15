8cc99380eaa612d8f0b2b7f1564c6007458ef570
dragos-niculescu <dragos-niculescu@users.noreply.github.com>
2018-10-15 16:16:11 +0300

    Update print_flags.c

--- print_flags.c
+++ print_flags.c
@@ -12,11 +12,11 @@ uint32_t flagmask[] = {
   0x0010, // auxiliary
   0x0040, // zero
   0x0080, // sign
-  0x0800 // overflow
+  0x0800  // overflow
 };
 char flagletters[] = "CPAZSO"; // same order as above
-uint32_t a = 10;
-uint32_t b = -20;
+uint32_t a;
+uint32_t b;
 uint8_t result; 
 uint32_t eflags, i;
 char op; 
